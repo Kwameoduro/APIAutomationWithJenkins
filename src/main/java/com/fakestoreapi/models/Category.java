4e8179db9e9d47bd0c7de3cbdbf57f710b0fe7b7
Kwame Oduro <odurokwameee@gmail.com>
2025-08-14 17:09:07 +0000

    feat: add category model for cart resource

---
+++ src/main/java/com/fakestoreapi/models/Category.java
@@ -0,0 +1,36 @@
+package com.fakestoreapi.models;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Category {
+
+    @JsonProperty("category")
+    private String category;
+
+    public Category() {
+        // Default constructor needed for Jackson deserialization
+    }
+
+    public Category(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "category='" + category + '\'' +
+                '}';
+    }
+}
